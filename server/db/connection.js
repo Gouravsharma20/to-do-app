ca4731e65fbd0bfa3b8919c57d0feb3ccbd4ef82
gourav <gouravsharma20a@gmail.com>
2025-10-03 16:20:14 +0530

    Updated GroupForms, NoteForms, AppContext and added services + .env

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,8 +1,15 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Todo")
-.then(()=>{
-    console.log("Server is Connected to database")
-})
-.catch((err)=>{console.log`Error is ${err}`})
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/todoapp";
 
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+    console.log("Connected to database:", mongoURI.split('/').pop().split('?')[0]);
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
+
+module.exports = mongoose.connection;
