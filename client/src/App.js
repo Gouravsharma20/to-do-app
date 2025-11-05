8bc577597287f5fc6ff66dc43f2d1f6a5797dd7e
gourav <gouravsharma20a@gmail.com>
2025-11-05 17:49:17 +0530

    react to-do app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
-
-import './App.css';
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './Pages/Home/Home';
 
 
 function App() {
-  
-
   return (
     <Router>
       <Routes>
